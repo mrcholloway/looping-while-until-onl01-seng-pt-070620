42b31eafd1594598dd13c85e88a1ae5040e32b39
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-19 20:40:06 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,15 +1,5 @@
 def using_while
 	levitation_force = 6
-	while levitation_force < 10
-		puts "Wingardium Leviosa"
-		levitation_force += 1
-	end
-		puts "You did it! The feather is levitating!"
-end
-
-
-def using_while
-	levitation_force = 6
 
 	while levitation_force<10
     puts "Wingardium Leviosa"
