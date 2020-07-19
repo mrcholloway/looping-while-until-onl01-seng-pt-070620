782eef85bb44bc4a6c21530cf2bfcdfa7ceba5ef
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-19 20:39:59 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -8,3 +8,12 @@ def using_while
 end
 
 
+def using_while
+	levitation_force = 6
+
+	while levitation_force<10
+    puts "Wingardium Leviosa"
+    levitation_force+=1
+  end
+  puts "I can do anything"
+end
