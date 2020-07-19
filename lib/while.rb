9a7a1c310c642c7276baab62a79f68f03fccb874
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-19 20:51:23 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
 	levitation_force = 6
 	while levitation_force < 10
-	phrase
+	phrase = 
   puts "Wingardium Leviosa"
     levitation_force += 1
   end
@@ -12,3 +12,15 @@ end
 
 
 
+def while_iterator(number_of_times)
+  number_of_times = 0
+  while number_of_times < 7 
+
+  # code your solution here using the "while" keyword	  # code your solution here using the "while" keyword
+  # hint: user a counter to tell the while loop when to stop!	  # hint: user a counter to tell the while loop when to stop!
+  phrase = "Welcome to Flatiron School's Web Development Course!"	  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+  puts phrase
+  number_of_times += 1
+  end
+end	end
